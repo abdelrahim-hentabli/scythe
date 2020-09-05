e141687ce86483cc7993a20accfd50c9735614d4
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-09-04 22:22:44 -0700

    added faction constructor

--- src/faction.h
+++ src/faction.h
@@ -6,13 +6,18 @@
 class Faction{
   public:
     Faction();
-    Faction(int factionNumber);
-    Faction(std::string factionName);
+    Faction(std::string factionName, unsigned char factionPower, 
+    unsigned char factionCombatCards, unsigned char factionAbility,
+    unsigned char factionMechAbility[5], Hex* homeBase);
   private:
     std::string name;
-    int ability;
-    int mechAbilities[4];
-    bool deployedMech[4];
+    unsigned char power;
+    unsigned char combatCards;
+    unsigned char ability;
+
+    //riverwalk is first 2 abilities
+    unsigned char mechAbilities[5];
+    bool deployedMech[5];
     Hex* homeBase;
 };
 
