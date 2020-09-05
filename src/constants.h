b00027608d66e62d42c0c585a6f8851d5be60169
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-09-04 20:53:29 -0700

    created a list of all abilities

--- src/constants.h
+++ src/constants.h
@@ -3,6 +3,12 @@
 
 const unsigned char MAX_POWER = 16;
 const unsigned char MAX_POPULARITY = 18;
+const int NUMBER_OF_ABILITIES = 21;
+
+const std::string abilities[NUMBER_OF_ABILITIES] = {"Coercion", "Swim", "Meander", "Relentless", "Dominate", "River Walk -> Farm", 
+                                                    "River Walk -> Tundra", "River Walk -> Forest", "River Walk -> Mountain", 
+                                                    "River Walk -> Village", "Wayfare", "Scout", "Speed", "Seaworthy", "Artillery",
+                                                    "Submerge", "Camaraderie", "Township", "People's Army", "Underpass", "Disarm"};
 
 
 #endif
