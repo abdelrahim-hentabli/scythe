1a15a10f0930fa5823822b6ad8cd4630efe2902e
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-09-04 22:23:43 -0700

    added faction creation to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+
 #include "board.h"
+#include "faction.h"
+#include "constants.h"
 
 int main(int argc, char  *argv[]){
-  
+  Faction* factions[NUMBER_OF_FACTIONS];
+  for(int i=0; i < NUMBER_OF_FACTIONS; i++){
+    //hex needs to be updated
+  factions[i] = new Faction( FACTION_NAMES[i], FACTION_POWERS[i], 
+    FACTION_COMBAT_CARDS[i], FACTION_ABILITIES[i], 
+    FACTION_MECH_ABILITIES[i], new Hex());
+  }
+
+
   return 1;
 }
