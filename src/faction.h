e4e6258c4e43f6f0fb8bcc5e7e705f158c30716c
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-09-04 20:54:28 -0700

    added methods and variables to faction class

--- src/faction.h
+++ src/faction.h
@@ -1,11 +1,19 @@
 #ifndef FACTION_H
 #define FACTION_H
 
+#include "hex.h"
+
 class Faction{
   public:
-
+    Faction();
+    Faction(int factionNumber);
+    Faction(std::string factionName);
   private:
-    
+    std::string name;
+    int ability;
+    int mechAbilities[4];
+    bool deployedMech[4];
+    Hex* homeBase;
 };
 
 #endif
