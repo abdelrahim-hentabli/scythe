0126585f7e9ec1775dff8d1fa4b82bf86c856db1
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-09-13 15:02:01 -0700

    beautified faction mat printing

--- src/faction.h
+++ src/faction.h
@@ -5,32 +5,47 @@
 #include "hex.h"
 #include "constants.h"
 
+const int FACTION_MAT_SIZE = 80;
+
 class Faction{
   public:
     Faction();
     Faction(const std::string &factionName, const int &factionPower, 
       const int &factionCombatCards, const int &factionAbility,
       int *factionMechAbility, Hex* factionHomeBase);
-    friend std::ostream& operator << (std::ostream &out, const Faction& p){
-      out<<p.name<<'\n';
-      out<<"Power: "<<p.power<<"    Combat Cards: ";
-      out<<p.combatCards<<"    Main Ability: "<<ABILITIES[p.ability]<<'\n';
-      out<<"Mech Abilities:    ";
+    friend std::ostream& operator << (std::ostream &out, const Faction& p){ 
+      out<<"  "<<std::string(FACTION_MAT_SIZE - 4, '_')<<"\n";
+      
+      out<<" /"<<std::string( (FACTION_MAT_SIZE-4) / 3 - p.name.size() % 2 - p.name.size() / 2, ' ')\
+      <<p.name<<std::string((FACTION_MAT_SIZE-4) % 3 + (FACTION_MAT_SIZE-4) / 3 - p.name.size() / 2\
+      - ABILITIES[p.ability].size() / 2, ' ')<<ABILITIES[p.ability]<<std::string( (FACTION_MAT_SIZE-4)\
+      / 3- ABILITIES[p.ability].size() / 2 - ABILITIES[p.ability].size() % 2, ' ')<<"\\ \n";
+     
+      out<<"|"<<std::string((FACTION_MAT_SIZE -2), ' ')<<"|\n";
+      out<<"|"<<std::string(FACTION_MAT_SIZE - 22, ' ')<<"Power: "<<p.power\
+      <<std::string(12, ' ')<<"|\n";
+      
+      out<<"|"<<std::string((FACTION_MAT_SIZE - 22), ' ')<<"Combat Cards: "<<p.combatCards\
+      <<std::string(5, ' ')<<"|\n";
+      
+      out<<"|    Mech Abilities:"<<std::string(FACTION_MAT_SIZE - 21, ' ')<<"|\n";
+
+      std::string temp = "";
       for(int i = 0; i < 5; i++){
         if(i == 0){
-          out<<"River Walking -> ";
+          temp +="Rvr Wlk-> ";
         }
-        out<<ABILITIES[p.mechAbilities[i]];
+        temp +=ABILITIES[p.mechAbilities[i]];
         if(i == 0){
-          out<<" , ";
+          temp+=", ";
         }
         else if(i < 4){
-          out<<"  |  ";
-        }
-        else{
-          out<<'\n';
+          temp+=" | ";
         }
       }
+      out<<" \\ "<<std::string( (FACTION_MAT_SIZE - 6 - temp.size()) / 2 + (FACTION_MAT_SIZE -6- temp.size()) %2, ' ')<<temp\
+      <<std::string( (FACTION_MAT_SIZE - 6 - temp.size()) /2, ' ')<<" / \n";
+      return out<<"  "<<std::string(FACTION_MAT_SIZE-4, '-')<<"  ";
     }
   private:
     std::string name;
