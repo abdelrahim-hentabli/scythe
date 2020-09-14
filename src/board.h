904e509767a30837df7ba8afc70680ab01ba2a9e
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-09-14 15:04:36 -0700

    implemented the creation of the 2D array for hexes

--- src/board.h
+++ src/board.h
@@ -1,15 +1,16 @@
 #ifndef BOARD_H
 #define BOARD_H
+
 #include <iostream>
 #include <map>
 #include "hex.h"
 
 class Board{
   public:
-
-
+    Board();
+    
   private:
-    Hex *hexagons;
+    Hex **hexagons;
     std::map<Hex*, Hex*> edges;
 };
 #endif 
